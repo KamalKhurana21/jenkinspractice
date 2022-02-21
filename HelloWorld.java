ce662da130282ae2b1aaa1ed552935686a0cedfb
KamalKhurana21 <51366938+KamalKhurana21@users.noreply.github.com>
2022-02-21 19:06:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] atgs)
 	{
-		System.out.println("Hello world and welcome the kamal khurana 12");
+		System.out.println("Hello world and welcome the kamal khurana 22");
 	}
 }
